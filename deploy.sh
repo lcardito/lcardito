ac5166436a26e9b7a2f778cadc8b621481750867
Luigi Cardito <lcardito@gmail.com>
2017-04-28 13:47:26 +0100

    fix: building in script

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,12 @@ set -e
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 
+npm run build
+
 gh-pages -d build
+
 git checkout master
+
 git merge gh-pages -m "Deploy to GitHub pages [ci skip]" --allow-unrelated-histories
 git push --force --quiet
 
