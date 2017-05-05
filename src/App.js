28b8471ba865accd5fecc6412030072bfde88e98
Luigi Cardito <lcardito@gmail.com>
2017-05-05 15:03:44 +0100

    feat: initial description and tag cloud

--- src/App.js
+++ src/App.js
@@ -1,33 +1,102 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 import {Container, Grid, Header, Menu, Segment} from "semantic-ui-react";
+import {TagCloud} from "react-tagcloud";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="ui large top fixed hidden">
-          <Container>
-              <Menu pointing secondary={true}>
-                  <Menu.Item>Bio</Menu.Item>
-                  <Menu.Item>Experience</Menu.Item>
-              </Menu>
-              <div className="pusher">
-                  <Segment vertical className="masthead center aligned">
-                      <Container text textAlign="center">
-                          <Header as="h1">Luigi Cardito</Header>
-                          <Header as="h2">Software Engineer</Header>
-                      </Container>
-                  </Segment>
-              </div>
-          </Container>
-          <Segment className="vertical stripe">
-              <Grid container={true} stackable={true} verticalAlign="middle" textAlign="center">
+    render() {
+        const skills = [
+            {value: "Agile", count: 30},
+            {value: "Clean Code", count: 26},
+            {value: "TDD", count: 28},
+            {value: "Kanban", count: 27},
+            {value: "Scrum", count: 26},
+            {value: "DevOps", count: 22},
+            {value: "AWS", count: 23},
+            {value: "Continuous Integration", count: 21},
+            {value: "Continuous Deployment", count: 22},
+            {value: "Continuous Delivery", count: 23},
+            {value: "Java", count: 29},
+            {value: "Javascript", count: 25},
+            {value: "Node.js", count: 25},
+            {value: "Dropwizard", count: 23},
+            {value: "Ansible", count: 22},
+            {value: "GIT", count: 23},
+            {value: "Spring", count: 18},
+            {value: "Play! Framework", count: 17},
+            {value: "Python", count: 20},
+            {value: "Django", count: 19},
+            {value: "Docker", count: 28},
+            {value: "Bash", count: 22},
+            {value: "Maven", count: 26}
+        ];
 
-              </Grid>
-          </Segment>
-      </div>
-    );
-  }
+        const customRenderer = (tag, size, color) => (
+            <div key={tag.value}
+                 style={{
+                     animation: 'blinker 3s linear infinite',
+                     animationDelay: `${Math.random() * 2}s`,
+                     fontSize: `${size}em`,
+                     margin: '3px',
+                     padding: '3px',
+                     display: 'inline-block',
+                     color: color,
+                 }}>{tag.value}</div>
+        );
+
+        return (
+            <div className="ui large top fixed hidden">
+                <Container>
+                    <Menu pointing secondary={true}/>
+                    <div className="pusher">
+                        <Segment vertical className="masthead center aligned">
+                            <Container text textAlign="center">
+                                <Header as="h1">Luigi Cardito</Header>
+                                <Header as="h2">Software Engineer</Header>
+                            </Container>
+                        </Segment>
+                    </div>
+                </Container>
+                <Segment className="vertical stripe">
+                    <Grid container={true} stackable={true} verticalAlign="middle" textAlign="center">
+                        <Grid.Row>
+                            <Grid.Column width={10} className="wide">
+                                <Header as="h3">Who am I?</Header>
+                                <Container text textAlign="justified">
+                                    Pro-active, motivated, curious and passionate developer, always ready to grab new
+                                    exciting opportunities.
+                                    I love working in Agile environments, I strongly believe in TDD and Pair Programming
+                                    benefits, I'm a great team worker and
+                                    I like mentoring and listening to more experienced developers so that the team value
+                                    will increase quickly.
+                                </Container>
+                                <Header as="h3">What do I do?</Header>
+                                <Container text textAlign="justified">
+                                    <p>I fully embrace the DevOps culture, leading to the benefits of the Continuous
+                                        Delivery practises without compromising the quality of my work.
+                                        I respect and chase early feedback from users and stakeholders, reacting and
+                                        changing the requirements as soon as possible to deliver a better product every
+                                        time.</p>
+                                    <p>I also like to understand the road map and the features' priorities. I want to be
+                                        involved in the life-cycle of the product, not only the application,
+                                        in order to provide the best experience to our customers.</p>
+                                    <p>My main interests and expertises are: Engineering and Agile methodologies (TDD,
+                                        Scrum/Kanban, Pair Programming), REST APIs, Micro-services,
+                                        Java EE, Java Play! Framework, Spring, JavaScript (NodeJS, React, Flux).</p>
+                                </Container>
+                            </Grid.Column>
+                            <Grid.Column width={6} className="wide" floated="right">
+                                <TagCloud minSize={1}
+                                          maxSize={2}
+                                          tags={skills}
+                                          renderer={customRenderer}/>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Segment>
+            </div>
+        );
+    }
 }
 
 export default App;
