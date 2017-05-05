61e3631f9eb4d388a56abfefbc0d22cdb2acdcb6
Luigi Cardito <lcardito@gmail.com>
2017-05-05 11:55:14 +0100

    Rewrite can finally start.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Container, Grid, Header, Menu, Segment} from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome my dear!</h2>
-        </div>
-        <p className="App-intro">
-		    This is a work in progress as a re-write of a personal profile.
-        </p>
+      <div className="ui large top fixed hidden">
+          <Container>
+              <Menu pointing secondary={true}>
+                  <Menu.Item>Bio</Menu.Item>
+                  <Menu.Item>Experience</Menu.Item>
+              </Menu>
+              <div className="pusher">
+                  <Segment vertical className="masthead center aligned">
+                      <Container text textAlign="center">
+                          <Header as="h1">Luigi Cardito</Header>
+                          <Header as="h2">Software Engineer</Header>
+                      </Container>
+                  </Segment>
+              </div>
+          </Container>
+          <Segment className="vertical stripe">
+              <Grid container={true} stackable={true} verticalAlign="middle" textAlign="center">
+
+              </Grid>
+          </Segment>
       </div>
     );
   }
