594a509d296f7c03f59fb1e71ee60970dc367bdf
Luigi Cardito <lcardito@gmail.com>
2017-11-08 18:57:45 +0000

    feat: change this old description.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,26 +5,21 @@ const Intro = () => (
     <div>
         <Header as='h3'>Who am I</Header>
         <Container text textAlign='justified' fluid>
-            Pro-active, motivated, curious and passionate developer, always ready to grab new
-            exciting opportunities.
-            I love working in Agile environments, I strongly believe in TDD and Pair Programming
-            benefits, I'm a great team worker and
-            I like mentoring and listening to more experienced developers so that the team value
-            will increase quickly.
+            <p>I am an Agile Software Developer , currently building Java solution pushing for a DevOps culture.</p>
         </Container>
         <Header as='h3'>What do I do</Header>
         <Container text textAlign='justified' fluid>
-            <p>I fully embrace the DevOps culture, leading to the benefits of the Continuous
-                Delivery practises without compromising the quality of my work.
-                I respect and chase early feedback from users and stakeholders, reacting and
-                changing the requirements as soon as possible to deliver a better product every
-                time.</p>
-            <p>I also like to understand the road map and the features' priorities. I want to be
-                involved in the life-cycle of the product, not only the application,
-                in order to provide the best experience to our customers.</p>
-            <p>My main interests and expertise are: Engineering and Agile methodologies (TDD,
-                Scrum/Kanban, Pair Programming), REST APIs, Micro-services,
-                Java EE, Java Play! Framework, Spring, Dropwizard, JavaScript (NodeJS, React, Redux).</p>
+            <p>I am an ambitious, curious and engaged software developer. I've started my professional career back in 2011/2012 contributing to
+                web projects based on Java / Javascript technologies.</p>
+
+            <p>I do believe in the "T-shaped developer" / DevOps by which I mean that developers teams must be autonomous in setting up,
+                deploying and supporting their applications (Continuous Integration and Continuous Delivery are a given) with the help and
+                the guidance of experts in infrastructures, networking and host managements where possible and preferably in the cloud.</p>
+
+            <p>My tech environment of choice would still be a backend, Java 8 micro-services (Spring boot, Dropwizard, Ratpack).
+                I've successfully delivered decently sized front-end applications using React and before it, AngularJS as well.
+                All the applications / services I've built follow the CI/CD patterns and best practises mostly wrapped in docker containers.
+                I enjoy a good build and deployment pipeline (Jenkins / GoCD) together with a configuration management tool (Ansible).</p>
         </Container>
     </div>
 );
