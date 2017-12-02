a59093b9a373e53ecd211ce4299e7422d6a35334
luigicardito <lcardito@gmail.com>
2017-12-02 16:34:56 +0000

    Google analytics

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./components/App";
+import ReactGA from 'react-ga';
+
+
+if (process.env.NODE_ENV !== 'development') {
+    ReactGA.initialize('UA-110494248-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
